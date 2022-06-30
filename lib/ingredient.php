30ea2720e9b7ccadc2ef4ed6ed9aa1d1f413e1be
Sarah <sarahwafaa27@gmail.com>
2022-06-30 13:32:32 +0200

    Update Ingredient (#9)

--- lib/ingredient.php
+++ lib/ingredient.php
@@ -17,17 +17,34 @@ class ingredient {
 
     public function selecteerIngredient ($gerecht_id) {
         $sql= "SELECT * FROM ingredient where gerecht_id = $gerecht_id";
+
+        $return  = [];
     
         $result = mysqli_query($this->connection, $sql);
         while($ingredient = mysqli_fetch_array($result)) {
-            echo "<pre>";
-            $artikel = $this->selecteerArtikel($ingredient["artikel_id"]);
-            echo "--------------------------------------------------------";
-            var_dump($artikel);
 
-        }       
+            $artikel = $this->selecteerArtikel($ingredient["artikel_id"]);
+          
+            $return[] = [
+                "ingredient_id" => $ingredient["id"],
+                "artikel_id" => $artikel["id"],
+                "artikel_naam" => $artikel["naam"],
+                "artikel_omschrijving" => $artikel["omschrijving"],
+                "artikel_prijs" => $artikel["prijs"],
+                "artikel_cal" => $artikel["calorie"],
+                "artikel_eh" => $artikel["eenheid"],
+                "artikel_verpak" => $artikel["verpakking"],
+                "aantal" => $ingredient["aantal"],
+                "gerecht-id" =>$ingredient ["gerecht_id"]
+            ];
+
+        }      
+        
+        
+        return($return);
     
-        return([]);
+    
+
     }
 
 }
