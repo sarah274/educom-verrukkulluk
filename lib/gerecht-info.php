32a27ab25d974ef5e69f9352f2bcae48ef5fa098
Sarah <sarahwafaa27@gmail.com>
2022-07-11 09:30:46 +0200

    #8 Update

--- lib/gerecht-info.php
+++ lib/gerecht-info.php
@@ -23,40 +23,44 @@ class gerecht_info {
 
     public function selectgerechtInfo ($gerecht_id, $record_type) {
        
-       $sql= "SELECT * FROM gerecht_info where gerecht_id = $gerecht_id and record_type = '$record_type' ";     
-
-
-        $return= [];
+       $sql= "SELECT * FROM gerecht_info where gerecht_id = $gerecht_id and record_type = '$record_type'";     
 
         $result = mysqli_query($this->connection, $sql);
 
-        while($gerecht_info = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
+        
+
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
         {
 
           if ($record_type == "O" || $record_type == "F") {
-            $user= $this->selecteerUser($gerecht_info ["user_id"]);
-            
-            echo "<pre>";
+            $user= $this->selecteerUser($row["user_id"]);
 
-            $GI[] = [
-            "gerecht_info_id" => $gerecht_info ["id"],
-            "record_type" => $gerecht_info ["record_type"],
-            "gerecht_id" => $gerecht_info ["gerecht_id"],
-            "user_id" => $user["id"],
-            "datum" => $gerecht_info ["datum"],
-            "nummers" => $gerecht_info ["nummeriekveld"],
-            "tekst" => $gerecht_info ["tekstveld"],
-            "name"=> $user ["user_name"],
-            "foto" => $user ["afbeelding"],
-            "email"=> $user ["email"],
-            "password"=> $user ["password"]
-            ];
 
+            $gerecht_info[] = ["gerecht_info" => $row,
+                                "user_info" => $user];
+
+            // $GI[] = [
+            // "gerecht_info_id" => $gerecht_info ["id"],
+            // "record_type" => $gerecht_info ["record_type"],
+            // "gerecht_id" => $gerecht_info ["gerecht_id"],
+            // "user_id" => $user["id"],
+            // "datum" => $gerecht_info ["datum"],
+            // "nummers" => $gerecht_info ["nummeriekveld"],
+            // "tekst" => $gerecht_info ["tekstveld"],
+            // "name"=> $user ["user_name"],
+            // "foto" => $user ["afbeelding"],
+            // "email"=> $user ["email"],
+            // "password"=> $user ["password"]
+            // ];
+
+        } else 
+            {
+            $gerecht_info[] = $row;
             }
 
         }
 
-    return($GI);
+    return($gerecht_info);
 
     }
 
